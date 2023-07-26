08750065cc32cd8767320d7710e0b92f3590e148
Arun Vishnu <arunvishnu@Aruns-MacBook-Pro.local>
2023-07-25 17:43:44 -0700

    initial commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { createCharts } from './charts.js';
 
 // const URL = 'https://raw.githubusercontent.com/arunvishnun/narrative-visualization/main/data/gdp-growth-data.csv';
-const URL = '../data/data.json';
+const URL = 'https://raw.githubusercontent.com/arunvishnun/narrative-visualization/main/data/data.json';
 
 (async function () {
     const data = await d3.json(URL);
