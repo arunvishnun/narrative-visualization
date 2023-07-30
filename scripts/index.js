8f24c67fd95a2886d03ccdd3cd7c6afe4273adf7
Arun Vishnu <arunvishnu@Aruns-MacBook-Pro.local>
2023-07-29 17:46:45 -0700

    clean up

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ import { cleanData, extractFullDataAndFormat } from './data-conversion.js';
 
 const RUN_DATA_CLEAN = false;
 
-// const URL = 'https://raw.githubusercontent.com/arunvishnun/narrative-visualization/main/data/data.json';
-const URL = '../data/full-data.json';
+const URL = 'https://raw.githubusercontent.com/arunvishnun/narrative-visualization/main/data/full-data.json';
+// const URL = '../data/full-data.json';
 
 (async function () {
     // set RUN_DATA_CLEAN to true Only to run once during developemnt to play around with data as its a large data set.
